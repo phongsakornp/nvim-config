840fecfff2b2fc661aacb5aaeb58f06de296ee48
phongsakornp <misterphongs@gmail.com>
2024-09-04 15:40:31 +0700

    feat: change open current file in file explore behavior

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -17,7 +17,7 @@ return {
 
     nvimtree.setup {
       view = {
-        width = 35,
+        width = 45,
         relativenumber = true,
       },
       -- change folder arrow icons
@@ -56,7 +56,7 @@ return {
     local keymap = vim.keymap -- for conciseness
 
     keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
-    keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
+    keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFile<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
     keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
     keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
   end,
