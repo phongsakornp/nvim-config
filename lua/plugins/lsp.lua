86be0df159fc70f4ac9c19085ce22d4f262d2ab0
phongsakornp <misterphongs@gmail.com>
2024-09-18 22:30:26 +0700

    feat: change lsp key map

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -147,8 +147,8 @@ return {
           opts.desc = "Show LSP implementations"
           keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
 
-          opts.desc = "Show LSP type definitions"
-          keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
+          -- opts.desc = "Show LSP type definitions"
+          -- keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
 
           opts.desc = "See available code actions"
           keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
@@ -322,7 +322,7 @@ return {
         format_on_save = {
           lsp_fallback = true,
           async = false,
-          timeout_ms = 1000,
+          timeout_ms = 5000,
         },
       }
 
