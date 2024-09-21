ec7b6925a1028b15916519f57583cb18ba44d2cb
phongsakornp <misterphongs@gmail.com>
2024-09-21 10:52:11 +0700

    feat: config nvim-tree lazy to false

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -7,6 +7,8 @@
 
 return {
   "nvim-tree/nvim-tree.lua",
+  version = "*",
+  lazy = false,
   dependencies = "nvim-tree/nvim-web-devicons",
   config = function()
     local nvimtree = require "nvim-tree"
